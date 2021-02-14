8825f84a5c4db5eb68776cfaedda8ad93a81ebf7
Oliver Pöckelhofer <oliver.poeckelhofer@hotmail.com>
2021-02-14 02:41:18 +0100

    Fixed background sound looping by having intro separate

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class AudioController : MonoBehaviour
 {
     public static AudioController Instance;
-
+        
     [SerializeField]
     AudioSource backgroundAudio;
+    [SerializeField]
+    AudioClip backgroundLoop;
 
     [SerializeField]
     AudioSource sfxAudio;
 
     private void Awake()
     {
-        Instance = this;
+        Instance = this;       
+    }
+
+    private void Update()
+    {
+        // start main background loop once intro has finished
+        if(!backgroundAudio.isPlaying)
+        {
+            backgroundAudio.clip = backgroundLoop;
+            backgroundAudio.loop = true;
+            backgroundAudio.Play();
+        }
     }
 
     public void PlayClip(AudioClip clip)
